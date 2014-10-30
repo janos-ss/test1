647b661d15eebe8c456c0181b722b19c570b43af
G. Ann Campbell <ganncamp@gmail.com>
2014-10-30 11:22:01 -0400

    corrected license header

--- src/test/java/com/sonarsource/ruleapi/domain/RuleTest.java
+++ src/test/java/com/sonarsource/ruleapi/domain/RuleTest.java
@@ -1,9 +1,7 @@
 /*
- *
- *  * Copyright (C) 2014 SonarSource SA
- *  * All rights reserved
- *  * mailto:contact AT sonarsource DOT com
- *
+ * Copyright (C) 2014 SonarSource SA
+ * All rights reserved
+ * mailto:contact AT sonarsource DOT com
  */
 package com.sonarsource.ruleapi.domain;
 
