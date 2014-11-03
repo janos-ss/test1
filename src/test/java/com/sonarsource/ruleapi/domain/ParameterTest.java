2e8e7bbc820de5cf122462cb0c3d986887edea32
G. Ann Campbell <ganncamp@gmail.com>
2014-11-03 15:22:52 -0500

    added tests

---
+++ src/test/java/com/sonarsource/ruleapi/domain/ParameterTest.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2014 SonarSource SA
+ * All rights reserved
+ * mailto:contact AT sonarsource DOT com
+ */
+
+package com.sonarsource.ruleapi.domain;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+public class ParameterTest extends TestCase {
+
+  private Parameter full;
+
+  @Override
+  protected void setUp() throws Exception
+  {
+    super.setUp();
+    full = new Parameter();
+    full.setKey("fullKey");
+    full.setDescription("This is the description of the fully-described parameter");
+    full.setType("Text");
+    full.setDefaultVal("*.*");
+  }
+
+  public void testEqualsSameRef() throws Exception {
+
+    Assert.assertTrue(full.equals(full));
+  }
+
+  public void testEqualsSameValues () throws Exception {
+    Parameter copy = new Parameter();
+    copy.setDefaultVal(full.getDefaultVal());
+    copy.setKey(full.getKey());
+    copy.setDescription(full.getDescription());
+
+    Assert.assertTrue(full.equals(copy));
+  }
+
+  public void testEqualsNeqKey() throws Exception {
+    Parameter copy = new Parameter();
+    copy.setDefaultVal("copyKey");
+    copy.setKey(full.getKey());
+    copy.setDescription(full.getDescription());
+
+    Assert.assertFalse(full.equals(copy));
+  }
+
+  public void testEqualsNeqDescription () throws Exception {
+    Parameter copy = new Parameter();
+    copy.setDefaultVal(full.getDefaultVal());
+    copy.setKey(full.getKey());
+    copy.setDescription("New description");
+
+    Assert.assertFalse(full.equals(copy));
+  }
+
+
+  public void testEqualsNeqDefault () throws Exception {
+    Parameter copy = new Parameter();
+    copy.setDefaultVal("**/*.*");
+    copy.setKey(full.getKey());
+    copy.setDescription(full.getDescription());
+
+    Assert.assertFalse(full.equals(copy));
+  }
+}
