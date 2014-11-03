fbc0a92563ce534e5ed03fbfe1b0c20aa4c04c04
G. Ann Campbell <ganncamp@gmail.com>
2014-11-03 15:33:12 -0500

    added tests

--- src/test/java/com/sonarsource/ruleapi/domain/ParameterTest.java
+++ src/test/java/com/sonarsource/ruleapi/domain/ParameterTest.java
@@ -65,4 +65,38 @@ public class ParameterTest extends TestCase {
 
     Assert.assertFalse(full.equals(copy));
   }
+
+  public void testEqualsNeqNullDefault () throws Exception {
+    Parameter copy = new Parameter();
+    copy.setKey(full.getKey());
+    copy.setDescription(full.getDescription());
+
+    Assert.assertFalse(full.equals(copy));
+  }
+
+  public void testEqualsNeqOtherNullDefault () throws Exception {
+    Parameter copy = new Parameter();
+    copy.setKey(full.getKey());
+    copy.setDescription(full.getDescription());
+
+    Assert.assertFalse(copy.equals(full));
+  }
+
+  public void testHashCodeDefault () throws Exception {
+    Parameter copy = new Parameter();
+    copy.setKey(full.getKey());
+    copy.setDescription(full.getDescription());
+
+    Assert.assertEquals(-1130130840, full.hashCode());
+  }
+
+  public void testHashCodeNullDefault () throws Exception {
+    Parameter copy = new Parameter();
+    copy.setKey(full.getKey());
+    copy.setDescription(full.getDescription());
+
+    Assert.assertEquals(-1130172670, copy.hashCode());
+  }
+
+
 }
