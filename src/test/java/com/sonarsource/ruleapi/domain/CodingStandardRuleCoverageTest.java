1f40dcf552928f0adc599e08911cc479ed2a84eb
agigleux <gigleux_alexandre@yahoo.fr>
2015-01-02 15:41:07 +0100

    add missing header

--- src/test/java/com/sonarsource/ruleapi/domain/CodingStandardRuleCoverageTest.java
+++ src/test/java/com/sonarsource/ruleapi/domain/CodingStandardRuleCoverageTest.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) 2014 SonarSource SA
+ * All rights reserved
+ * mailto:contact AT sonarsource DOT com
+ */
 package com.sonarsource.ruleapi.domain;
 
 import static org.fest.assertions.Assertions.assertThat;
