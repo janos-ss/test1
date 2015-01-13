1e5e778c592fec9e83bf1b89245c055933726c88
G. Ann Campbell <ganncamp@gmail.com>
2015-01-13 12:10:18 +0100

    updated test

--- src/test/java/com/sonarsource/ruleapi/externalspecifications/specifications/FindbugsTest.java
+++ src/test/java/com/sonarsource/ruleapi/externalspecifications/specifications/FindbugsTest.java
@@ -37,8 +37,8 @@ public class FindbugsTest {
     fb.populateRulesCoverageMap();
     fb.computeCoverage();
 
-    assertThat(fb.implementable).isEqualTo(400);
-    assertThat(fb.skipped).isEqualTo(23);
+    assertThat(fb.implementable).isEqualTo(397);
+    assertThat(fb.skipped).isEqualTo(26);
     assertThat(fb.specified).isEqualTo(0);
     assertThat(fb.implemented).isEqualTo(0);
 
