39bcf12f11db87197b39fee08df145f4db82f2e3
G. Ann Campbell <ganncamp@gmail.com>
2015-01-19 16:14:26 +0100

    Added reports option Added JCommander for CLI arg management

--- src/main/java/com/sonarsource/ruleapi/Main.java
+++ src/main/java/com/sonarsource/ruleapi/Main.java
@@ -6,9 +6,17 @@
 
 package com.sonarsource.ruleapi;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import com.sonarsource.ruleapi.services.IntegrityEnforcementService;
 import com.sonarsource.ruleapi.domain.RuleException;
+import com.sonarsource.ruleapi.services.ReportService;
+import com.sonarsource.ruleapi.services.RuleManager;
+import org.fest.util.Strings;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -23,23 +31,92 @@ public class Main {
 
   public static void main(String [] args) {
 
-    if (args.length < 2) {
-      LOGGER.severe("Username, password required as first, second arguments.");
-      System.exit(-1);
+    Settings settings = new Settings();
+    new JCommander(settings, args);
+
+    List<Option> options = new ArrayList<Option>();
+    for (String str : settings.option) {
+
+      Option option = Option.fromString(str);
+      if (option == null) {
+        option = Option.REPORTS;
+      }
+      if (! options.contains(option)) {
+        options.add(option);
+      }
     }
 
-    String login = args[0];
-    String password = args[1];
+    for (Option option : options) {
+      doRequestedOption(option, settings);
 
-    IntegrityEnforcementService enforcer = new IntegrityEnforcementService();
+    }
+  }
+
+  protected static void doRequestedOption(Option option, Settings settings) {
+
+
+    IntegrityEnforcementService enforcer = null;
+    if (option != Option.REPORTS) {
+
+      if (Strings.isNullOrEmpty(settings.login) || Strings.isNullOrEmpty(settings.password)) {
+        throw new ParameterException("-login and -password required for integrity option");
+      }
+      enforcer = new IntegrityEnforcementService();
+    }
+
     try {
-      enforcer.setCoveredAndOutdatedLanguages(login, password);
-      enforcer.enforceCwe(login, password);
+      switch (option) {
+        case OUTDATED :
+          enforcer.setCoveredAndOutdatedLanguages(settings.login, settings.password);
+         break;
+
+        case INTEGRITY :
+          enforcer.enforceCwe(settings.login, settings.password);
+          break;
+
+        case REPORTS:
+        default:
+          ReportService rs = new ReportService();
+          rs.getReports(settings.instance);
+          break;
+
+      }
     } catch (RuleException e) {
       LOGGER.log(Level.SEVERE, e.getMessage(), e);
     }
   }
 
 
+  public static class Settings{
+
+    @Parameter(required = true)
+    private List<String> option;
+
+    @Parameter(names = "-instance")
+    private String instance = RuleManager.NEMO;
+
+    @Parameter(names = "-login")
+    private String login;
+
+    @Parameter(names = "-password")
+    private String password;
+
+  }
+
+  public enum Option {
+    REPORTS, OUTDATED, INTEGRITY;
+
+    public static Option fromString(String input) {
+
+      for(Option output : Option.values()) {
+        if(output.toString().equalsIgnoreCase(input)) {
+          return output;
+        }
+      }
+
+      return null;
+    }
+
+  }
 
 }
