6f0efcfc39aa586740d8a2cecc247feeceb8dc72
G. Ann Campbell <ganncamp@gmail.com>
2015-01-30 09:10:44 -0500

    beefed up Javadoc

--- src/main/java/com/sonarsource/ruleapi/externalspecifications/DerivativeTaggableStandard.java
+++ src/main/java/com/sonarsource/ruleapi/externalspecifications/DerivativeTaggableStandard.java
@@ -12,7 +12,15 @@ import java.util.Map;
 
 /**
  * A DerivativeTaggableStandard is one that is based on another standard.
- * E.G. SANS Top 25 is derived from, and references CWE
+ *
+ * E.G. SANS Top 25 is derived from, and references CWE, and because
+ * SANS Top 25 is not stand-alone, different algorithms may be required
+ * for it than for other standards.
+ *
+ * For instance the CWE standard can/should enforce that every rule
+ * with a value in the CWE field has a 'cwe' tag, but SANS is also
+ * based on the CWE field and should NOT add a 'sans-top25' tag
+ * to every rule with a CWE value.
  */
 public interface DerivativeTaggableStandard extends TaggableStandard {
 
