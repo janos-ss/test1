ddd1c506dffd5b5d7952fe340c4caf8ed1222b1a
G. Ann Campbell <ganncamp@gmail.com>
2015-02-09 14:35:50 -0500

    fixed screwed up tests

--- src/test/java/com/sonarsource/ruleapi/ReportGenerator.java
+++ src/test/java/com/sonarsource/ruleapi/ReportGenerator.java
@@ -14,41 +14,41 @@ import com.sonar.orchestrator.Orchestrator;
 
 public class ReportGenerator {
 
-//	@Test
-//	public void generateReports() {
-//		Orchestrator orchestrator = Orchestrator
-//				.builderEnv()
-//				.setOrchestratorProperty("sonar.runtimeVersion", "LTS")
-//				.setOrchestratorProperty("orchestrator.updateCenterUrl",
-//						"http://update.sonarsource.org/update-center-dev.properties")
-//				.setOrchestratorProperty("sonar.jdbc.dialect", "h2")
-//				.setOrchestratorProperty("cobolVersion", "DEV").addPlugin("cobol")
-//				.setOrchestratorProperty("javaVersion", "DEV").addPlugin("java")
-//				.setOrchestratorProperty("javascriptVersion", "DEV").addPlugin("javascript")
-//				.setOrchestratorProperty("cppVersion", "DEV").addPlugin("cpp")
-//				.setOrchestratorProperty("webVersion", "DEV").addPlugin("web")
-//				.setOrchestratorProperty("plsqlVersion", "DEV").addPlugin("plsql")
-//				.setOrchestratorProperty("phpVersion", "DEV").addPlugin("php")
-//				.setOrchestratorProperty("csharpVersion", "DEV").addPlugin("csharp")
-//				.setOrchestratorProperty("groovyVersion", "DEV").addPlugin("groovy")
-//				.setOrchestratorProperty("abapVersion", "DEV").addPlugin("abap")
-//				.setOrchestratorProperty("flexVersion", "DEV").addPlugin("flex")
-//				.setOrchestratorProperty("pliVersion", "DEV").addPlugin("pli")
-//				.setOrchestratorProperty("pythonVersion", "DEV").addPlugin("python")
-//				.setOrchestratorProperty("rpgVersion", "DEV").addPlugin("rpg")
-//				.setOrchestratorProperty("vbVersion", "DEV").addPlugin("vb")
-//				.setOrchestratorProperty("vbnetVersion", "DEV").addPlugin("vbnet")
-//				.setOrchestratorProperty("xmlVersion", "DEV").addPlugin("xml")
-//				.build();
-//		orchestrator.start();
-//
-//    ReportService reportService = new ReportService();
-//    try {
-//      reportService.getReports(orchestrator.getServer().getUrl());
-//    } catch (RuleException e) {
-//      e.printStackTrace();
-//    }
-//
-//		orchestrator.stop();
-//	}
+	@Test
+	public void generateReports() {
+		Orchestrator orchestrator = Orchestrator
+				.builderEnv()
+				.setOrchestratorProperty("sonar.runtimeVersion", "LTS")
+				.setOrchestratorProperty("orchestrator.updateCenterUrl",
+						"http://update.sonarsource.org/update-center-dev.properties")
+				.setOrchestratorProperty("sonar.jdbc.dialect", "h2")
+				.setOrchestratorProperty("cobolVersion", "DEV").addPlugin("cobol")
+				.setOrchestratorProperty("javaVersion", "DEV").addPlugin("java")
+				.setOrchestratorProperty("javascriptVersion", "DEV").addPlugin("javascript")
+				.setOrchestratorProperty("cppVersion", "DEV").addPlugin("cpp")
+				.setOrchestratorProperty("webVersion", "DEV").addPlugin("web")
+				.setOrchestratorProperty("plsqlVersion", "DEV").addPlugin("plsql")
+				.setOrchestratorProperty("phpVersion", "DEV").addPlugin("php")
+				.setOrchestratorProperty("csharpVersion", "DEV").addPlugin("csharp")
+				.setOrchestratorProperty("groovyVersion", "DEV").addPlugin("groovy")
+				.setOrchestratorProperty("abapVersion", "DEV").addPlugin("abap")
+				.setOrchestratorProperty("flexVersion", "DEV").addPlugin("flex")
+				.setOrchestratorProperty("pliVersion", "DEV").addPlugin("pli")
+				.setOrchestratorProperty("pythonVersion", "DEV").addPlugin("python")
+				.setOrchestratorProperty("rpgVersion", "DEV").addPlugin("rpg")
+				.setOrchestratorProperty("vbVersion", "DEV").addPlugin("vb")
+				.setOrchestratorProperty("vbnetVersion", "DEV").addPlugin("vbnet")
+				.setOrchestratorProperty("xmlVersion", "DEV").addPlugin("xml")
+				.build();
+		orchestrator.start();
+
+    ReportService reportService = new ReportService();
+    try {
+      reportService.getReports(orchestrator.getServer().getUrl());
+    } catch (RuleException e) {
+      e.printStackTrace();
+    }
+
+		orchestrator.stop();
+	}
 }
