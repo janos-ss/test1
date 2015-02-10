e1faa4359afb4ee17eaed10956d14531368e6101
G. Ann Campbell <ganncamp@gmail.com>
2015-02-10 08:39:07 -0500

    removed reimplementations of String methods(Doh!)

--- src/main/java/com/sonarsource/ruleapi/utilities/MarkdownConverter.java
+++ src/main/java/com/sonarsource/ruleapi/utilities/MarkdownConverter.java
@@ -152,7 +152,7 @@ public class MarkdownConverter {
     if (line.matches("[^\\[]*\\[[^|]+\\|https?[A-Za-z0-9-._~:/?#\\\\[\\\\]@!$&'()*+,;=% ]+\\].*")) {
       int pos = line.indexOf("http");
       int hrefStart = findBefore(line, pos, '[');
-      int hrefEnd = findAfter(line, pos, ']');
+      int hrefEnd = line.indexOf(']', pos+1);
 
       String href = line.substring(pos, hrefEnd);
       String label = line.substring(hrefStart + 1, pos - 1);
@@ -214,13 +214,13 @@ public class MarkdownConverter {
     }
 
     if (line.startsWith("||")) {
-      line = "<tr><th>" + line.substring(2, findLast(line, '|') - 1) + "</th></tr>";
+      line = "<tr><th>" + line.substring(2, line.lastIndexOf('|') - 1) + "</th></tr>";
       line = line.replaceAll("\\|\\|","|");
       line = line.replace("|", "</th><th>");
       paragraph = false;
 
     } else if (line.startsWith("|")) {
-      line = "<tr><td>" + line.substring(1, findLast(line, '|')) + "</td></tr>";
+      line = "<tr><td>" + line.substring(1, line.lastIndexOf('|')) + "</td></tr>";
       line = line.replace("|", "</td><td>");
       paragraph = false;
     }
@@ -261,7 +261,7 @@ public class MarkdownConverter {
   }
 
   protected String handleList(StringBuilder sb, String line) {
-    int pos = findFirst(line, ' ');
+    int pos = line.indexOf(' ');
 
     if (line.matches("[*#]+ +.*")) {
 
@@ -288,7 +288,7 @@ public class MarkdownConverter {
   }
 
   private String handleLi(String line) {
-    int pos = findFirst(line, ' ');
+    int pos = line.indexOf(' ');
     paragraph = false;
     return "<li>" + line.substring(pos) + "</li>";
   }
@@ -328,36 +328,9 @@ public class MarkdownConverter {
     return line;
   }
 
-  private int findAfter(String line, int start, char ch) {
-    for (int i = start + 1; i < line.length(); i++) {
-      if (line.charAt(i) == ch) {
-        return i;
-      }
-    }
-    return -1;
-  }
-
   private int findBefore(String line, int start, char ch) {
     for (int i = start - 1; i >= 0; i--) {
       if (line.charAt(i) == ch) {
-        return i;
-      }
-    }
-    return -1;
-  }
-
-  private int findLast(String line, char ch) {
-    for (int i = line.length() - 1; i > -1; i--) {
-      if (line.charAt(i) == ch) {
-        return i;
-      }
-    }
-    return -1;
-  }
-
-  private int findFirst(String src, char charToFind) {
-    for (int i = 0; i < src.length(); i++) {
-      if (src.charAt(i) == charToFind) {
         return i;
       }
     }
