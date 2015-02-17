d6b3e73e32045648aa2093c3db5ac1fdd2a68498
G. Ann Campbell <ganncamp@gmail.com>
2015-02-17 12:05:28 -0500

    added test

--- src/test/java/com/sonarsource/ruleapi/domain/CodingStandardRuleCoverageTest.java
+++ src/test/java/com/sonarsource/ruleapi/domain/CodingStandardRuleCoverageTest.java
@@ -10,7 +10,30 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class CodingStandardRuleCoverageTest {
 
+
+  @Test
+  public void testGetRuleKeysAsString() {
+
+    List<Rule> rules = new ArrayList<Rule>();
+    Rule rule = new Rule("");
+    rule.setKey("RSPEC-1234");
+    rules.add(rule);
+
+    rule = new Rule("");
+    rule.setKey("RSPEC-2345");
+    rules.add(rule);
+
+    CodingStandardRuleCoverage cov = new CodingStandardRuleCoverage();
+
+    String expected = "RSPEC-1234, RSPEC-2345";
+    assertThat(cov.getRuleKeysAsString(rules)).isEqualTo(expected);
+
+  }
 
 }
