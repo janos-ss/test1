730b820617577b88c28f17f70472e1cf2b5f43c4
G. Ann Campbell <ganncamp@gmail.com>
2015-03-10 15:56:43 -0400

    added tests

--- src/test/java/com/sonarsource/ruleapi/services/RuleManagerTest.java
+++ src/test/java/com/sonarsource/ruleapi/services/RuleManagerTest.java
@@ -38,4 +38,28 @@ public class RuleManagerTest {
     assertThat(map.size()).isEqualTo(2);
     assertThat(map.get("key1")).isEqualTo(r1);
   }
+
+  @Test
+  public void testGetNormalKey() {
+
+    String key = "RSPEC-123";
+    assertThat(manager.getNormalKey(key, Language.ABAP)).isEqualTo(key);
+
+  }
+
+  @Test
+  public void testStandardizeKeysAndIdentifyMissingRspecs() {
+
+    Rule rule = new Rule("Java");
+    rule.setKey("RSPEC-123");
+
+    List<Rule> sqCovered = new ArrayList<Rule>();
+    sqCovered.add(rule);
+
+    List notFound = manager.standardizeKeysAndIdentifyMissingSpecs(Language.JAVA, sqCovered);
+
+    assertThat(notFound).isEmpty();
+
+  }
+
 }
