dcdffa232b545b0156e55001b5ff395f248e82f1
G. Ann Campbell <ganncamp@gmail.com>
2015-06-15 07:40:31 -0400

    fixed broken test

--- src/test/java/com/sonarsource/ruleapi/externalspecifications/specifications/SansTop25Test.java
+++ src/test/java/com/sonarsource/ruleapi/externalspecifications/specifications/SansTop25Test.java
@@ -157,9 +157,9 @@ public class SansTop25Test {
     String expectedSummaryReport = newline +
             "SANS Top 25" + newline +
             "Insecure Interaction Between Components  6, unimplementable:  0  specified:  0,  implemented:  0" + newline +
-            "Porous Defenses                         11, unimplementable:  3  specified:  0,  implemented:  0" + newline +
+            "Porous Defenses                         11, unimplementable:  2  specified:  0,  implemented:  0" + newline +
             "Risky Resource Management                8, unimplementable:  1  specified:  1,  implemented:  1" + newline +
-            "Total                                   25, unimplementable:  4  specified:  1,  implemented:  1" + newline;
+            "Total                                   25, unimplementable:  3  specified:  1,  implemented:  1" + newline;
     String summaryReport = "";
     summaryReport = sans.getSummaryReport("");
 
