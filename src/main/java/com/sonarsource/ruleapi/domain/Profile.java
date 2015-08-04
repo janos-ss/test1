ef8086d8b9dbf3684777e53fc3e28dde2691a037
G. Ann Campbell <ganncamp@gmail.com>
2015-08-04 09:39:18 -0400

    dropped unneeded setters

--- src/main/java/com/sonarsource/ruleapi/domain/Profile.java
+++ src/main/java/com/sonarsource/ruleapi/domain/Profile.java
@@ -31,19 +31,9 @@ public class Profile implements Comparable<Profile> {
     return key;
   }
 
-  public void setKey(String key) {
-
-    this.key = key;
-  }
-
   public String getName() {
 
     return name;
-  }
-
-  public void setName(String name) {
-
-    this.name = name;
   }
 
   @Override
