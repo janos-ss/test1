838ace1eeb77faaee7e2b05c43cd2f6e982d57a7
G. Ann Campbell <ganncamp@gmail.com>
2015-08-25 09:46:14 -0400

    removed bad test

--- src/test/java/com/sonarsource/ruleapi/services/IntegrityEnforcementServiceTest.java
+++ src/test/java/com/sonarsource/ruleapi/services/IntegrityEnforcementServiceTest.java
@@ -26,13 +26,6 @@ public class IntegrityEnforcementServiceTest {
   IntegrityEnforcementService enforcer = new IntegrityEnforcementService();
 
   @Test
-  public void testtest(){
-    ReportService rs = new ReportService();
-    rs.writeOutdatedRulesReport(Language.CSH, RuleManager.NEMO);
-  }
-
-
-  @Test
   public void testDropEmptyMapEntries(){
     Map <Rule, Map<String,Object>> map = new HashMap<>();
     map.put(new Rule(""), new HashMap<String, Object>());
