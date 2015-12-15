41f94e4b53da1ae30d84a02c3a54f12dfbb9e64f
G. Ann Campbell <ganncamp@gmail.com>
2015-12-15 08:02:22 -0500

    RULEAPI-342 upgrade parent pom & update license headers

--- src/main/java/com/sonarsource/ruleapi/externalspecifications/AbstractMisraSpecification.java
+++ src/main/java/com/sonarsource/ruleapi/externalspecifications/AbstractMisraSpecification.java
@@ -1,9 +1,8 @@
 /*
- * Copyright (C) 2014 SonarSource SA
+ * Copyright (C) 2014-2016 SonarSource SA
  * All rights reserved
  * mailto:contact AT sonarsource DOT com
  */
-
 package com.sonarsource.ruleapi.externalspecifications;
 
 import com.sonarsource.ruleapi.domain.CodingStandardRuleCoverage;
--- src/main/java/com/sonarsource/ruleapi/externalspecifications/package-info.java
+++ src/main/java/com/sonarsource/ruleapi/externalspecifications/package-info.java
@@ -1,9 +1,8 @@
 /*
- * Copyright (C) 2014 SonarSource SA
+ * Copyright (C) 2014-2016 SonarSource SA
  * All rights reserved
  * mailto:contact AT sonarsource DOT com
  */
-
 /**
  * Created by ganncamp on 12/15/14.
  */
