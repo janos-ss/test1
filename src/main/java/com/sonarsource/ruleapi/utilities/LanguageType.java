fbd6c62bbe5b310a34411bf1a72328f32177bf17
G. Ann Campbell <ganncamp@gmail.com>
2016-02-22 14:18:09 -0500

    RULEAPI-363 missing header

--- src/main/java/com/sonarsource/ruleapi/utilities/LanguageType.java
+++ src/main/java/com/sonarsource/ruleapi/utilities/LanguageType.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) 2014-2016 SonarSource SA
+ * All rights reserved
+ * mailto:contact AT sonarsource DOT com
+ */
 package com.sonarsource.ruleapi.utilities;
 
 public enum LanguageType { LEGACY, STRONG, LOOSE}
--- src/test/java/com/sonarsource/ruleapi/externalspecifications/specifications/RuleSpecTest.java
+++ src/test/java/com/sonarsource/ruleapi/externalspecifications/specifications/RuleSpecTest.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) 2014-2016 SonarSource SA
+ * All rights reserved
+ * mailto:contact AT sonarsource DOT com
+ */
 package com.sonarsource.ruleapi.externalspecifications.specifications;
 
 import com.sonarsource.ruleapi.domain.Rule;
