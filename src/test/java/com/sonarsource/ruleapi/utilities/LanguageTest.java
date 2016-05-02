dab4d78f2df873666557eabf7bc7b4836742bb95
G. Ann Campbell <ganncamp@gmail.com>
2016-05-02 09:24:18 -0400

    add tests

--- src/test/java/com/sonarsource/ruleapi/utilities/LanguageTest.java
+++ src/test/java/com/sonarsource/ruleapi/utilities/LanguageTest.java
@@ -5,6 +5,9 @@
  */
 package com.sonarsource.ruleapi.utilities;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 import org.junit.Test;
 
 import static org.fest.assertions.Assertions.assertThat;
@@ -31,6 +34,35 @@ public class LanguageTest {
     assertThat(Language.fromString("Orange")).isNull();
     assertThat(Language.fromString("JavaScript")).isEqualTo(Language.JS);
     assertThat(Language.fromString("Python")).isEqualTo(Language.PY);
+  }
+
+  @Test
+  public void languageTypeMembership(){
+
+    for (Language lang : Language.LEGACY_LANGUAGES) {
+      assertThat(Language.LOOSLY_TYPE_LANGUAGES).excludes(lang);
+      assertThat(Language.STRONGLY_TYPED_LANGUAGES).excludes(lang);
+    }
+
+    for (Language lang : Language.STRONGLY_TYPED_LANGUAGES) {
+      assertThat(Language.LOOSLY_TYPE_LANGUAGES).excludes(lang);
+      assertThat(Language.LEGACY_LANGUAGES).excludes(lang);
+    }
+
+    for (Language lang : Language.LOOSLY_TYPE_LANGUAGES) {
+      assertThat(Language.LEGACY_LANGUAGES).excludes(lang);
+      assertThat(Language.STRONGLY_TYPED_LANGUAGES).excludes(lang);
+    }
+
+    List<Language> langs = new ArrayList<>(Language.values().length);
+    langs.addAll(Language.LEGACY_LANGUAGES);
+    langs.addAll(Language.LOOSLY_TYPE_LANGUAGES);
+    langs.addAll(Language.STRONGLY_TYPED_LANGUAGES);
+
+    assertThat(langs).excludes(Language.XML);
+    assertThat(langs).excludes(Language.WEB);
+    assertThat(langs.size()).isEqualTo(Language.values().length - 2);
+
   }
 
 }
