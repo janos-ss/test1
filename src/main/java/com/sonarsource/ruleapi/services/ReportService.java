9eead42e68b9800a7dddd0530047a16bbf152b6a
Hirle <eric.hirlemann@sonarsource.com>
2016-07-07 11:26:52 +0200

    RULEAPI-420 Fix quality flaw

--- src/main/java/com/sonarsource/ruleapi/services/ReportService.java
+++ src/main/java/com/sonarsource/ruleapi/services/ReportService.java
@@ -33,8 +33,6 @@ import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Scanner;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 
