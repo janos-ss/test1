1b18ee9f7bc5b86efd4463378ece49766388d404
Hirle <eric.hirlemann@sonarsource.com>
2016-09-14 09:40:28 +0200

    RULEAPI-418 Fix quality flaw

--- src/main/java/com/sonarsource/ruleapi/services/RuleFilesService.java
+++ src/main/java/com/sonarsource/ruleapi/services/RuleFilesService.java
@@ -73,6 +73,7 @@ public class RuleFilesService {
 
   public void generateRuleFiles(Iterable<String> ruleKeys) {
     countGeneratedFiles = 0;
+    int countRulesProcessed = 0;
     if (ruleKeys != null) {
       List<Rule> updatedRules = new ArrayList<>();
       for (String ruleKey : ruleKeys) {
@@ -82,8 +83,10 @@ public class RuleFilesService {
         }
         generateSingleRuleFiles(rule);
         updatedRules.add(rule);
-        if (countGeneratedFiles % 20 == 0) {
-          System.out.println(String.format("%4d rules processed",countGeneratedFiles/2));
+
+        countRulesProcessed++;
+        if (countRulesProcessed % 10 == 0) {
+          System.out.println(String.format("%4d rules processed",countRulesProcessed ));
         }
       }
       updateProfiles(updatedRules);
