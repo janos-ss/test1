8d1952ec93d272a99f78fc575a3a5cac48731a0e
Janos Gyerik <janos.gyerik@sonarsource.com>
2017-08-10 09:37:53 +0200

    Add some issues

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -4,10 +4,25 @@ package com.mycompany.app;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+  /**
+   * Constructor
+   *
+   * @param name
+   */
+  public App(String name) {
+  }
+
+  /**
+   * name
+   *
+   * @return name
+   */
+  public String getName() {
+    return "hello";
+  }
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
 }
