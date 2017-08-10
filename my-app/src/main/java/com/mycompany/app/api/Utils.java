203ebb34f257f60c915d1ab702ad7a8314115478
Janos Gyerik <janos.gyerik@sonarsource.com>
2017-08-10 12:13:13 +0200

    Add class with documentation issues

---
+++ my-app/src/main/java/com/mycompany/app/api/Utils.java
@@ -0,0 +1,34 @@
+package com.mycompany.app.api;
+
+/**
+ * A utility class
+ */
+public class Utils {
+
+  public void undocumented() {
+    // no-op
+  }
+
+  /**
+   *
+   */
+  public void documentWithoutWords() {
+    // no-op
+  }
+
+  /**
+   * singleWord
+   */
+  public void documentWithOneWord() {
+    // no-op
+  }
+
+  /**
+   * two words
+   */
+  public void documentWithTwoWords() {
+    // no-op
+  }
+
+  // TODO
+}
