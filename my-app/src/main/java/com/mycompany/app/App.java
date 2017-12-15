74368c86fb61850fd5eb0cac53d19ccf28b50e18
Janos Gyerik <janos.gyerik@sonarsource.com>
2017-12-15 17:58:40 +0100

    .

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -1,8 +1,4 @@
 package com.mycompany.app;
-// TODO
-// TODO
-// TODO
-// TODO
 
 /**
  * Hello world!
@@ -29,5 +25,19 @@ public class App {
 
   public static void main(String[] args) {
     System.out.println("Hello World!");
+    System.out.println("Hello World! 2");
+    System.out.println("Hello World! 3");
+    System.out.println("Hello World! 4");
+    System.out.println("Hello World! 5");
+    System.out.println("Hello World! 6");
+    System.out.println("Hello World! 7");
+    System.out.println("Hello World! 8");
+  }
+
+  /**
+   * some JavaDoc
+   */
+  public String foo() {
+    return getName();
   }
 }
