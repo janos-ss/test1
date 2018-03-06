273e77c1c67a7a2950fcb4798b5c2130cfe64bf2
Jean-Baptiste Lièvremont <jb.lievremont@gmail.com>
2018-03-06 16:16:56 +0100

    RULEAPI-484 Add support for Go language (#76)
    * RULEAPI-484 Add initial support for Go language
    
    * Update travis-utils

--- src/test/java/com/sonarsource/ruleapi/utilities/LanguageTest.java
+++ src/test/java/com/sonarsource/ruleapi/utilities/LanguageTest.java
@@ -18,7 +18,7 @@ public class LanguageTest {
   @Test
   public void testLanguagesEnum() {
 
-    assertThat(Language.values().length).isEqualTo(21);
+    assertThat(Language.values().length).isEqualTo(22);
     assertThat(Language.ABAP.sq).isEqualTo("abap");
     assertThat(Language.ABAP.rspec).isEqualTo("ABAP");
     assertThat(Language.ABAP.getSqCommon()).isEqualTo("common-abap");
